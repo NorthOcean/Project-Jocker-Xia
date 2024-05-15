458b327ec3cb5c3e07b24b36be2fd0765baeb565
conghaowong <conghaowong@icloud.com>
2024-05-15 10:41:15 +0800

    Update qpid and main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2022-06-20 15:28:14
 @LastEditors: Conghao Wong
-@LastEditTime: 2024-03-18 16:04:40
+@LastEditTime: 2024-05-14 20:31:41
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
@@ -14,36 +14,18 @@ import torch
 
 import qpid
 import socialCircle
-from scripts.utils import get_value
 
 torch.autograd.set_detect_anomaly(True)
 
 
 def main(args: list[str], run_train_or_test=True):
-    h_value = None
+    min_args = qpid.args.Args(args, is_temporary=True)
 
-    if '--help' in args:
-        h_value = get_value('--help', args, default='all_args')
-    elif '-h' in args:
-        h_value = get_value('-h', args, default='all_args')
-
-    if h_value:
-        from qpid.mods import vis
-        qpid.print_help_info(h_value)
+    if (h := min_args.help) != 'null':
+        qpid.print_help_info('all_args' if h == 'True' else h)
         exit()
 
-    min_args = qpid.args.Args(terminal_args=args,
-                              is_temporary=True)
-
-    model = min_args.model
-    if model == 'linear':
-        s = qpid.applications.Linear
-    elif model == 'static':
-        s = qpid.applications.Static
-    else:
-        s = qpid.silverballers.SILVERBALLERS_DICT.get_structure(model)
-
-    t = s(args)
+    t = qpid.get_structure(min_args.model)(args)
 
     if run_train_or_test:
         t.train_or_test()
@@ -53,6 +35,7 @@ def main(args: list[str], run_train_or_test=True):
         t.print_info_all()
 
     return t
+
 
 if __name__ == '__main__':
     main(sys.argv)
